a86f555ddc73307c3663b48e5cf4294d5479205f
motoha <motoha2010@gmail.com>
2024-06-10 03:43:23 +0700

    add auth module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,8 @@
+ 
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
- 
-import  * as NFC from 'nfc-pcsc';
-
+import { NFC } from '../../pcsc';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,23 +12,23 @@ export class HomeComponent implements OnInit {
   private nfc: any;
   constructor(
     private router: Router) {
-    // this.nfc = new NFC();
-    // this.nfc.on('reader', this.handleReader.bind(this)); 
+    this.nfc = new NFC();
+    this.nfc.on('reader', this.handleReader.bind(this)); 
   }
 
-    // handleReader(reader: any) {
-    //   console.log(`${reader.reader.name} reader detected`);
+    handleReader(reader: any) {
+      console.log(`${reader.reader.name} reader detected`);
   
-    //   reader.on('card', this.handleCard.bind(this));
-    //   reader.on('card.off', this.handleCardRemoved.bind(this));
-    // } handleCard(card: any) {
-    //   console.log(`Card detected: ${card.uid}`);
-    //   // Perform further operations with the card data
-    // }
+      reader.on('card', this.handleCard.bind(this));
+      reader.on('card.off', this.handleCardRemoved.bind(this));
+    } handleCard(card: any) {
+      console.log(`Card detected: ${card.uid}`);
+      // Perform further operations with the card data
+    }
   
-    // handleCardRemoved() {
-    //   console.log('Card removed');
-    // }
+    handleCardRemoved() {
+      console.log('Card removed');
+    }
     
   ngOnInit(): void {
     console.log('HomeComponent INIT');
---
+++ src/pcsc/indexxx.ts
@@ -0,0 +1,70 @@
+// import pcsc from "@pokusew/pcsclite";
+// import { EventEmitter } from "events";
+// import ACR122 from "./utils/readers/ACR122";
+// import Reader from "./utils/readers/Reader";
+
+// type PCSCEvents = {
+// 	reader: ACR122 | Reader;
+// 	error: Error;
+// };
+
+// export type Card = {
+// 	atr: Buffer;
+// 	standard: string;
+// 	uid?: string;
+// 	data?: Buffer;
+// };
+
+// import { KEYS, MODES, TAGS } from "./utils/readers/helpers/TMK";
+// export { Reader, ACR122, KEYS, MODES, TAGS };
+
+// export default class PCSC extends EventEmitter {
+// 	private pcsc = pcsc();
+
+// 	constructor() {
+// 		super();
+
+// 		this.pcsc.on("reader", (reader) => {
+// 			if (
+// 				reader.name.toLowerCase().indexOf("acr122") !== -1 ||
+// 				reader.name.toLowerCase().indexOf("acr125") !== -1
+// 			) {
+// 				const device = new ACR122(reader);
+// 				this.emit("reader", device);
+// 				return;
+// 			}
+
+// 			const device = new Reader(reader);
+// 			this.emit("reader", device);
+// 		});
+
+// 		this.pcsc.on("error", (err) => {
+// 			this.emit("error", err);
+// 		});
+// 	}
+
+// 	on<K extends keyof PCSCEvents>(
+// 		event: K,
+// 		listener: (v: PCSCEvents[K]) => void
+// 	): this {
+// 		return super.on(event, listener);
+// 	}
+
+// 	emit<K extends keyof PCSCEvents>(event: K, arg: PCSCEvents[K]): boolean {
+// 		return super.emit(event, arg);
+// 	}
+
+// 	get readers() {
+// 		//@ts-ignore: This does exist, but it's not in the typings for some reason
+// 		return this.pcsc.readers;
+// 	}
+
+// 	close() {
+// 		try {
+// 			this.pcsc.close();
+// 			return true;
+// 		} catch (error: any) {
+// 			throw new Error(error.message);
+// 		}
+// 	}
+// }
